36ccf65cc43c6f9b4bc441f1588ff4eae777f656
sparshsinghjaduvanshi <sparshsinghjaduvanshi@gmail.com>
2026-06-01 00:45:45 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+ apiKey: "AIzaSyCvwn56wM4ZqvlTgPLFPiUtGUZCggli60o",
+  authDomain: "yourtube-712db.firebaseapp.com",
+  projectId: "yourtube-712db",
+  storageBucket: "yourtube-712db.firebasestorage.app",
+  messagingSenderId: "1094620927908",
+  appId: "1:1094620927908:web:4a1a437166b5eabe851331",
+  measurementId: "G-GBX70S6XMD"
 };
 
 // Initialize Firebase
